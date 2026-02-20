5c75462dc4b5f1561bdd00ff5a4c080f9feb74e6
Kristofer Liljeblad <krist00fer@users.noreply.github.com>
2026-02-20 14:48:17 +0000

    Update hoist to 0.7.0

--- Formula/hoist.rb
+++ Formula/hoist.rb
@@ -1,22 +1,22 @@
 class Hoist < Formula
   desc "Configuration-as-code CLI for Azure AI Search and Microsoft Foundry"
   homepage "https://github.com/mklab-se/hoist"
-  version "0.6.1"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mklab-se/hoist/releases/download/v#{version}/hoist-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "e3c05ff9ef6f18ae4a2ef1aa80737b70b5e80a8f9ec0e569a800c26b0bc82b08"
+      sha256 "e5fa7d8ae06cc8ead057a7396f676b31deddace2347b7180c93b35517098440d"
     else
       url "https://github.com/mklab-se/hoist/releases/download/v#{version}/hoist-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "7777aa146f278b31d0f34bf58fa7901f16009098f9d41758ea9473bd7d91d397"
+      sha256 "007890b92b57aeaf3152a52b0a7d9442c94ab1bedd9c9ac61c38c4f7c44ed062"
     end
   end
 
   on_linux do
     url "https://github.com/mklab-se/hoist/releases/download/v#{version}/hoist-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "8bfb6b64d8c201c7e4b41fe372c51687aaeedb375d259d650b0305403d2df064"
+    sha256 "61173352714848e74a2004571688b2818c6aa09d074ca07fe5526634d6da6ff7"
   end
 
   def install
