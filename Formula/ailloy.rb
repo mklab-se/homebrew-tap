71e7858aa62974aa69b03da33a182e4095c6c3c0
Kristofer Liljeblad <krist00fer@users.noreply.github.com>
2026-03-03 19:16:29 +0100

    Update ailloy to 0.1.0

--- Formula/ailloy.rb
+++ Formula/ailloy.rb
@@ -7,16 +7,16 @@ class Ailloy < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mklab-se/ailloy/releases/download/v#{version}/ailloy-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "1a7b27be80481e664346ec01cc672aa000e41c7e174e8d21c170889f5f6ff5ed"
+      sha256 "46ab5d6b4c64099b5a6af0f77b84a36afb0bf48a69ee0f8d958fb8bb37f209eb"
     else
       url "https://github.com/mklab-se/ailloy/releases/download/v#{version}/ailloy-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "3161ea49579ba52e364994e2ca05125d31e191ed8a41ecbbcd583c747717f1ac"
+      sha256 "25cd95115099b22607af4de75bc9ccd28cae53ecd20546c735eb8b0a577167a7"
     end
   end
 
   on_linux do
     url "https://github.com/mklab-se/ailloy/releases/download/v#{version}/ailloy-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "f8c8c7132f3b4d37f4a1d6abadd6c2e9d67d129abf799d7ecd3876db697a43c0"
+    sha256 "e87b22a92747dd9cbfaaaac59adc1ac2fc5745e4858eee04ea174f5eb4731e45"
   end
 
   def install
