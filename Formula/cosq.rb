b870c5c817a7e70408e4d4f214e04194f40d9a1d
Kristofer Liljeblad <krist00fer@users.noreply.github.com>
2026-03-05 23:33:12 +0000

    Update cosq to 0.7.0

--- Formula/cosq.rb
+++ Formula/cosq.rb
@@ -1,22 +1,22 @@
 class Cosq < Formula
   desc "A CLI to query your Azure Cosmos DB instances"
   homepage "https://github.com/mklab-se/cosq"
-  version "0.6.1"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mklab-se/cosq/releases/download/v#{version}/cosq-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "612b91e31fa5d9d0106edad726920657dedd49ad5b3e5d4dbdadc7b43fca034c"
+      sha256 "28569056f6eaa0b14b56035c0549a67729d0d8e31801caebb66ca68b81a1e06a"
     else
       url "https://github.com/mklab-se/cosq/releases/download/v#{version}/cosq-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "4234121dd04be54c83cc2d1a77ae142f205ac40dcbdb2894702b04f3ed458a2a"
+      sha256 "5188ebad35d67ee29d87a8a8351e65255b271648617bbe8167367ab479617d5c"
     end
   end
 
   on_linux do
     url "https://github.com/mklab-se/cosq/releases/download/v#{version}/cosq-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "5e06f6616df91e608c02237af5cc1cd21605d00509eba2feb72722f5626aaf27"
+    sha256 "43797df22faae8ccff5879c71b9e6dbdb453fb8aaef7d6e8083400093d8352cd"
   end
 
   def install
