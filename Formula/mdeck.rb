d4417ba75cde2f4a8e4649bd136cda9a5920b758
Kristofer Liljeblad <krist00fer@users.noreply.github.com>
2026-05-18 15:07:47 +0000

    Update mdeck to 0.17.2

--- Formula/mdeck.rb
+++ Formula/mdeck.rb
@@ -1,22 +1,22 @@
 class Mdeck < Formula
   desc "A markdown-based presentation tool"
   homepage "https://github.com/mklab-se/mdeck"
-  version "0.17.1"
+  version "0.17.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mklab-se/mdeck/releases/download/v#{version}/mdeck-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "0f852d483eb35a0fbcdf36059292c7662fdd78b63c53b713bdcb063c078cee36"
+      sha256 "8dcd7c96ad40596553c49b174d7a3411e10f3ee8513a70c73e970934a88cf979"
     else
       url "https://github.com/mklab-se/mdeck/releases/download/v#{version}/mdeck-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "f928705f4b9f7343d76c91c9cefa53e71059f82b3e0245e2c889c7f3b5551e1e"
+      sha256 "ad7f3743e7b919200bfbc7c667ef73f524bdb739d73803ae2497cb9629f60bc7"
     end
   end
 
   on_linux do
     url "https://github.com/mklab-se/mdeck/releases/download/v#{version}/mdeck-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c61cd967177f2b9e7cb277e7f34e20b775df9ddf4289acf764458fa21e72ac4f"
+    sha256 "2995c635393b6873957fff758ca05f6cf0c0f27bac7ee9e114c02deeb1266321"
   end
 
   def install
