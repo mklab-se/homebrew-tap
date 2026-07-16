8e497d274f4e8704990080c87c8fb39a1da9e33b
Kristofer Liljeblad <krist00fer@users.noreply.github.com>
2026-07-16 11:20:47 +0200

    Update ailloy to 2.0.2

--- Formula/ailloy.rb
+++ Formula/ailloy.rb
@@ -1,22 +1,22 @@
 class Ailloy < Formula
   desc "An AI abstraction layer for Rust"
   homepage "https://github.com/mklab-se/ailloy"
-  version "2.0.1"
+  version "2.0.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mklab-se/ailloy/releases/download/v#{version}/ailloy-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "e485e18d8e25d7c5329eda07ab46378be73005b7b72949d19e5cf7b053b63dd7"
+      sha256 "24a5203560c89a0294e10fe569c8d571579566b770e651f78d5c53c4f27a1489"
     else
       url "https://github.com/mklab-se/ailloy/releases/download/v#{version}/ailloy-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "edb0a1ca8b25313c8211d2f7b1b5949bad43e4cdee2202023a45585a7e3df8b7"
+      sha256 "7105bc8539f4ac8681f59ad8130f7fe4a23812055d6fd0f5fa7081ea37f5504c"
     end
   end
 
   on_linux do
     url "https://github.com/mklab-se/ailloy/releases/download/v#{version}/ailloy-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "6dfc804b2e21d82a61a8d118d65a02020398e15770e97d324dcce90dd4dac5f3"
+    sha256 "843069834a6e4815ed5bc46c4479d2ea0042e0214b9c67eee4dbfba214a11a40"
   end
 
   def install
