5bbca533864e4f6694b03bb42b2c187bdc66d14c
Kristofer Liljeblad <krist00fer@users.noreply.github.com>
2026-09-06 22:09:21 +0000

    Update deemer to 0.2.1

--- Formula/deemer.rb
+++ Formula/deemer.rb
@@ -1,22 +1,22 @@
 class Deemer < Formula
   desc "Run AI-assisted integration tests that judge whether your tests passed"
   homepage "https://github.com/mklab-se/deemer"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mklab-se/deemer/releases/download/v#{version}/deemer-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "02c15bf20d5e3681f0b4bc0388f887ed6d832b3dc03d9d30523a05cae317c885"
+      sha256 "9d3c25907eae05f1b927672ed7ea75c36f781efb7062e8c53fc4b55b5ddffbb7"
     else
       url "https://github.com/mklab-se/deemer/releases/download/v#{version}/deemer-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "cbd86f17dd88b62c34b7aad810026fcb964a0383677b2aece2e652055cc43b9c"
+      sha256 "d6f3ed95daa7c1f238ffe8dcb33b8cfaabd629a028accb35192e7373b97ac0c9"
     end
   end
 
   on_linux do
     url "https://github.com/mklab-se/deemer/releases/download/v#{version}/deemer-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "f32402ac749dd59bd80c395cdaddf45753836423b9b154f2baeaac27d6d07546"
+    sha256 "bc1a1be0e4d343399c109cd8f92c970c4dfd9b52f1cec0206a753275dc681853"
   end
 
   def install
