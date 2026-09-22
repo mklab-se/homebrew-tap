75f9335a3c2921b582c6e63b56a58e332613ff80
Kristofer Liljeblad <krist00fer@users.noreply.github.com>
2026-09-22 16:06:47 +0000

    Update pidge to 1.3.0

--- Formula/pidge.rb
+++ Formula/pidge.rb
@@ -1,22 +1,22 @@
 class Pidge < Formula
   desc "A fast CLI for e-mail and calendar"
   homepage "https://github.com/mklab-se/pidge"
-  version "1.2.0"
+  version "1.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mklab-se/pidge/releases/download/v#{version}/pidge-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "cc7dab7bb145be9c438a8904d1d908b844a4b3589487608035ac008e019fe799"
+      sha256 "e36e5195da188c11d5f55c5d69be6567341a810b728e2bd1ed95e2400cf4894b"
     else
       url "https://github.com/mklab-se/pidge/releases/download/v#{version}/pidge-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "b6c77a4bd8134f77e8436b5fdc03f79e8039f04b58d6a592023239444254237b"
+      sha256 "eb92aaf582e17c067dd0335eb2969ca55258aa423a6fdd510f575a65bf732719"
     end
   end
 
   on_linux do
     url "https://github.com/mklab-se/pidge/releases/download/v#{version}/pidge-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "35217454893f33a2ac74bbfb65289deb270043faea80552b5ce7a7103784085e"
+    sha256 "457902b76f0d844e5e5ee40a17deb9894d05015ac45b2711c5520e82c0b637d4"
   end
 
   def install
